4b01ef854f35b52e1a23fed6cfd13669cd0cd8e5
AitorBR <44008436+AitorBR@users.noreply.github.com>
2018-10-10 00:57:56 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+        // comndos: psvm
+
+
+        System.out.println("Hola mundo crueeeeeel");
+        //sout
+
+    }
+
+
+}
