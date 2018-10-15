c57608daa405dca16938fcb09f70bbfbb631331d
AitorBR <44008436+AitorBR@users.noreply.github.com>
2018-10-15 20:07:42 +0200

    Version 1.02

--- src/problemas9.java
+++ src/problemas9.java
@@ -5,11 +5,23 @@ public class problemas9 {
     public static void main(String[] args) {
 
         Scanner poner = new Scanner(System.in);
-        double x;
+        double x, y, operador;
 
         System.out.println("Pon un numero: ");
         x = poner.nextFloat();
+        System.out.println("Pon otro numero: ");
+        y = poner.nextFloat();
+        System.out.println("Pon un operador (+ (1), - (2), * (3), / (4): ");
+        operador = poner.nextFloat();
 
+        if (operador == 1) {
+            System.out.println(x + y);
+        } else if (operador == 2) {
+            System.out.println(x - y);
+        } else if (operador == 3) {
+            System.out.println(x * y);
+        } else
+            System.out.println(x / y);
 
     }
 }
