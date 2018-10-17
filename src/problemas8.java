949bce6fe957febd2534e6aaa4d48f460385c9fc
AitorBR <44008436+AitorBR@users.noreply.github.com>
2018-10-17 18:19:48 +0200

    Version 1.02

--- src/problemas8.java
+++ src/problemas8.java
@@ -5,24 +5,31 @@ public class problemas8 {
     public static void main(String[] args) {
 
         Scanner poner = new Scanner(System.in);
-        double a, b, c, x, y, z;
+        double a, b, c, y;
 
         System.out.println("Pon el valor a: ");
         a = poner.nextFloat();
-        System.out.println("Pon el valor a: ");
+        System.out.println("Pon el valor b: ");
         b = poner.nextFloat();
-        System.out.println("Pon el valor a: ");
+        System.out.println("Pon el valor c: ");
         c = poner.nextFloat();
 
-        x = (int) ((-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a));
         y = (b * b) - (4 * a * c);
-        z = (-b + Math.sqrt((b * b) - (4 * a * c)));
+        if (y < 0) {
+            System.out.println("No tiene solucion");
+        } else {
+            System.out.println((double) (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a));
+            System.out.println((double) (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a));
+        }
 
+        /*x, z, w*/
+        //x = (int) ((-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a));
+        //z = (-b + Math.sqrt((b * b) - (4 * a * c)));
+        //w = Math.sqrt(y);
 
-        System.out.println((double) (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a));
-        System.out.println(x);
+       /* System.out.println(x);
         System.out.println(y);
         System.out.println(z);
-
+        System.out.println(w);*/
     }
 }
